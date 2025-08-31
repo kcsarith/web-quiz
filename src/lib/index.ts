aff97545f76e09b18a31849149dd257f46fff7e2
kcsarith <krisnasarith@gmail.com>
2025-08-31 14:27:55 -0700

    fix #5: Create helper to merge payload to correct type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,15 @@
 import { PollyClient } from "@aws-sdk/client-polly";
 
 export const pollyClient = new PollyClient({ region: "us-east-1" });
+
+export function updateObject<T extends Record<PropertyKey, any>>(
+    target: T,
+    source: Record<PropertyKey, any>
+): T {
+    Object.keys(source).forEach((key) => {
+        if (key in target) {
+            target[key as keyof T] = source[key];
+        }
+    });
+    return target;
+}
