1cc0edb28763f78275c4d3fbe8815fa62fe30b63
kcsarith <krisnasarith@gmail.com>
2025-09-01 16:24:07 -0700

    fix #5: Add put method to update user prefs

--- src/app/api/prefs/[username]/route.ts
+++ src/app/api/prefs/[username]/route.ts
@@ -1,24 +1,41 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { promises as fs } from "fs";
 import path from "path";
-
-export async function GET(params: { url: string }) {
+import { UserPrefsType, defaultPrefs } from "@/types";
+import { readFileAndParseJson, updateObjectDeep } from "@/lib";
+async function getUserPrefByName(name: string): Promise<UserPrefsType> {
+    return defaultPrefs
+}
+export async function GET(request: NextRequest) {
     try {
-        const username: string = params.url.split("prefs/")[1]
-        const filePath: string = path.join(process.cwd(), "data", "prefs", `${username}.json`)
-        const fileContents: string = await fs.readFile(filePath, "utf-8")
-        const fileObject = JSON.parse(fileContents);
-        return NextResponse.json({
-            status: 200,
-            data: fileObject,
-            error: null
-        })
+        const username: string = request.url.split("prefs/")[1]
+        const filePath: string = path.join("data", "prefs", `${username}.json`)
+        const fileObject = readFileAndParseJson(filePath)
+        return NextResponse.json(fileObject)
     } catch (e) {
         return NextResponse.json({
-            status: 500,
-            data: null,
             error: e
-        })
+        }, { status: 500 })
 
+    }
+}
+
+export async function PUT(request: NextRequest) {
+    try {
+        const username: string = request.url.split("prefs/")[1]
+        const filePath = path.join("data", "prefs", `${username}.json`)
+        const body: Record<string, any> = await request.json();
+        const fileObject = await readFileAndParseJson(filePath) as UserPrefsType
+        const bodyKeys = Object.keys(body);
+        bodyKeys.forEach((bodyKey: string) => {
+            if (!(bodyKey in fileObject)) {
+                delete body[bodyKey]
+            }
+        })
+        const updatedObject: UserPrefsType = updateObjectDeep(fileObject, body)
+        fs.writeFile(filePath, JSON.stringify(updatedObject))
+        return NextResponse.json(updatedObject)
+    } catch (e) {
+        return NextResponse.json({ error: e }, { status: 500 })
     }
 }
